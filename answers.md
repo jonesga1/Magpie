cb2e1adeae3aecff06c1e4c95389bc7761766300
Gabriel Jones <gabrieljones@gabriels-mbp.dcsdk12.org>
2017-01-27 12:55:42 -0700

    Finish activity 2

--- answers.md
+++ answers.md
@@ -8,3 +8,13 @@ The weather is nice
 -you don't say?
 Do you know my brother?
 -why so negative?
+
+  
+ • What happens when more than one keyword appears in a string? Consider the string “My mother has a dog but no cat.” Explain how to prioritize responses in the reply method.
+
+ If statment will check which keyword comes first in the if statment. So in this case it checks for 'mother' before 'dog', so it will put the response for mother first. To prioritize simply organize the if statements in a proper order.
+
+ Question
+1. What happens when a keyword is included in another word? Consider statements like “I know all the state capitals” and “I like vegetables smothered in cheese.” Explain the problem with the responses to these statements.
+
+The code will read all characters and see where a keyword appears, even if it's inside another word. So in this case it will see the no in know. This of course gives a response which makes no sense in context, which is bad.
--- src/Magpie2.java
+++ src/Magpie2.java
@@ -38,6 +38,19 @@ public class Magpie2 {
 				|| statement.indexOf("sister") >= 0
 				|| statement.indexOf("brother") >= 0) {
 			response = "Tell me more about your family.";
+		} else if (statement.indexOf("cat") >= 0
+				|| statement.indexOf("dog") >= 0) {
+			response = "Tell me more about your pets.";
+		} else if (statement.indexOf("Mr. Padjen") >=0) {
+			response = "He sounds like a nice teacher.";
+		} else if (statement.indexOf("magpie") >=0) {
+			response = "Hey, that's me.";
+		} else if (statement.indexOf("yes") >=0) {
+			response = "Nice to see some positivity.";
+		} else if (statement.indexOf("maybe") >=0) {
+			response = "Not very certain? Can't compute.";
+		} else if (statement.trim().length() == 0) {
+			response = "Please input something";
 		} else {
 			response = getRandomResponse();
 		}
@@ -50,7 +63,7 @@ public class Magpie2 {
 	 * @return a non-committal string
 	 */
 	private String getRandomResponse() {
-		final int NUMBER_OF_RESPONSES = 4;
+		final int NUMBER_OF_RESPONSES = 6;
 		double r = Math.random();
 		int whichResponse = (int) (r * NUMBER_OF_RESPONSES);
 		String response = "";
@@ -63,8 +76,12 @@ public class Magpie2 {
 			response = "Do you really think so?";
 		} else if (whichResponse == 3) {
 			response = "You don't say.";
+		} else if (whichResponse == 4) {
+			response = "My favorite number is 7.";
+		} else if (whichResponse == 5) {
+			response = "Let me think about that.";
 		}
-
+ 
 		return response;
 	}
 }
